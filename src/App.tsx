5e8831bc5b154e70f22370ce899550a6619bd7ee
v1690117 <vladimir.veselov@phystech.edu>
2024-08-08 00:34:09 +0300

    try vibro

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let patterns = [
+  2000, //vibrate one time for 2 seconds
+  [2000, 1000, 2000, 1000, 2000, 1000, 2000],
+  [400, 200, 400, 200, 400, 200, 800, 200, 800, 200, 400, 200, 400, 200, 200, 200], //vibrate "Twinkle, Twinkle, Little Star"
+  [150, 50, 150, 50, 300, 100, 150, 50, 150, 50, 300, 100, 150, 50, 150, 50], //vibrate "Super Mario Bros" theme
+  [300, 200, 300, 200, 300, 400, 300, 200, 300, 200, 300, 400, 300, 200, 600, 200] //vibrate "Jingle Bells"
+];
+
+function vibrationPattern(index: number){
+  if (!window.navigator.vibrate){
+    alert("Your device does not support the Vibration API. Try on an Android phone!");
+  }
+  else {
+    window.navigator.vibrate(patterns[index]);
+  }
+}
+
 function App() {
+  const [index, setIndex] = useState(0);
+  const onLogoClick = () => {
+    setIndex(prev => prev +1);
+    vibrationPattern(index % patterns.length);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={onLogoClick}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
