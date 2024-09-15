3e12815a0ba2825be68e600d749d4db70b516dbc
v1690117 <vladimir.veselov@phystech.edu>
2024-09-15 13:35:40 +0300

    add vibration

--- src/views/Workout/Set/Set.tsx
+++ src/views/Workout/Set/Set.tsx
@@ -3,6 +3,23 @@ import {Checkbox, Input, Wrapper, Button, SetRow} from "./SetStyles";
 import {ISet} from "../../../model/ISet";
 import SetService from "../../../services/SetService";
 
+let patterns = [
+    2000, //vibrate one time for 2 seconds
+    [2000, 1000, 2000, 1000, 2000, 1000, 2000],
+    [400, 200, 400, 200, 400, 200, 800, 200, 800, 200, 400, 200, 400, 200, 200, 200], //vibrate "Twinkle, Twinkle, Little Star"
+    [150, 50, 150, 50, 300, 100, 150, 50, 150, 50, 300, 100, 150, 50, 150, 50], //vibrate "Super Mario Bros" theme
+    [300, 200, 300, 200, 300, 400, 300, 200, 300, 200, 300, 400, 300, 200, 600, 200] //vibrate "Jingle Bells"
+];
+
+function vibrationPattern(index: number){
+    if (!window.navigator.vibrate){
+        alert("Your device does not support the Vibration API. Try on an Android phone!");
+    }
+    else {
+        window.navigator.vibrate(patterns[index]);
+    }
+}
+
 interface SetProps {
     set: ISet;
 }
@@ -72,6 +89,7 @@ const Set: React.FC<SetProps> = (props) => {
         if(seconds === 0 && timer) {
             clearInterval(timer);
             setTimer(null);
+            vibrationPattern(0);
         }
     }, [seconds, timer]);
 
