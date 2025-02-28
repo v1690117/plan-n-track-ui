ee267f00b5fc46e0fbe4ce151db88c1b2e4cce64
v1690117 <v1690117@outlook.com>
2025-02-28 23:44:54 +0300

    add simple view for Exercise

---
+++ src/views/Exercise/ExerciseStyles.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 20px;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const HeadingTitle = styled.div`
+    gap: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 20px;
+`;
+
+export const HeadingToolbar = styled.div`
+    gap: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-right: 20px;
+`;
+
+export const ExerciseTitle = styled.h1`
+    font-size: 24px;
+`;
