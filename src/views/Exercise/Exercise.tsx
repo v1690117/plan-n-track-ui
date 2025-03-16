21b66901c93238d4183f062e4138a8d3e870acc1
v1690117 <v1690117@outlook.com>
2025-03-16 21:53:50 +0300

    split sets for series based on reps number

--- src/views/Exercise/Exercise.tsx
+++ src/views/Exercise/Exercise.tsx
@@ -3,6 +3,7 @@ import {Container} from "./ExerciseStyles";
 import useAppStore from "../../store/store.ts";
 import {useParams} from "react-router-dom";
 import {AxisOptions, Chart} from "react-charts";
+import {ISet} from "../../model/ISet.ts";
 
 type Set = {
     date: Date,
@@ -21,17 +22,32 @@ const Exercise: React.FC = () => {
     const loadSets = useAppStore(s => s.loadExerciseSets);
 
     const data: Series[] = useMemo(() => {
-        return [{
-            label: 'Load',
-            data: sets
-                .filter(s => !!s.workout?.date && !!s.load && s.completed)
-                .sort((a,b) => b.workout.date - a.workout.date)
-                .map(s => ({
-                    date: new Date(s.workout.date),
-                    load: s.load,
-                    radius: s.reps
-                }))
-        }];
+        const grouppedSets: { [key: string]: ISet[] } = {};
+        sets.forEach(s => {
+            if (!s.workout || !s.workout.date || !s.load || !s.completed) {
+                return;
+            }
+            if (!grouppedSets["" + s.reps]) {
+                grouppedSets["" + s.reps] = [];
+            }
+            grouppedSets[s.reps].push(s);
+        });
+        console.log(grouppedSets);
+        return Object.keys(grouppedSets).map((reps) => ({
+            label: reps,
+            data: grouppedSets[reps].map(s => ({
+                date: new Date(s.workout.date),
+                load: s.load,
+                radius: s.reps
+            }))
+        }));
+        // [{
+        //     label: 'Load',
+        //     data: sets
+        //         .filter(s => !!s.workout?.date && !!s.load && s.completed)
+        //         .sort((a, b) => b.workout.date - a.workout.date)
+
+        // }];
     }, [sets]);
 
 
@@ -62,21 +78,21 @@ const Exercise: React.FC = () => {
     return (
         <Container>
             {data?.length > 0 && data[0].data.length > 0 && <Chart
-            options={{
-                data,
-                primaryAxis,
-                secondaryAxes,
-                interactionMode: "closest",
-                tooltip: {
-                    render: (datum) => datum.focusedDatum?.originalDatum.radius
-                },
-                getDatumStyle: (datum) =>
-                    ({
-                        circle: { r: datum.originalDatum.radius },
+                options={{
+                    data,
+                    primaryAxis,
+                    secondaryAxes,
+                    interactionMode: "closest",
+                    // tooltip: {
+                    //     render: (datum) => datum.focusedDatum?.originalDatum.radius
+                    // },
+                    getDatumStyle: (datum) =>
+                        ({
+                            circle: {r: datum.originalDatum.radius},
 
-                    } as never),
-            }}
-        />}
+                        } as never),
+                }}
+            />}
         </Container>
     );
 };
